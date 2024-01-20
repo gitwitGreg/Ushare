a3ef46eef7e2c59924e6df2566405b2fbe79e91a
Greg <Godly@udel.edu>
2024-01-20 12:49:16 -0500

    Vercel Changes

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -1,9 +1,10 @@
 import { Outlet, Navigate } from 'react-router-dom'
-
+import { useContext } from 'react';
+import { AuthContext } from '@/context/AuthContext';
 
 
 const AuthLayout = () => {
-  const isAuthenticated = false;
+  const { isAuthenticated } = useContext(AuthContext);
   return (
     <>
       {isAuthenticated ? (
